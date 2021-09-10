9f5117ff182ec9507ac1b2047e8abe58420b45a2
daithinh <daithinh.uit@gmail.com>
2021-09-10 22:49:21 +0700

    Update getCityForecast.jsx

--- src/APIcalls/getCityForecast.jsx
+++ src/APIcalls/getCityForecast.jsx
@@ -1,6 +1,6 @@
 export const getCityForecast = async (query) => {
   const apiKey = `2b1dfd97968067e68d497a960d2585ca`;
-  const cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${apiKey}`;
+  const cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&lang=vi&appid=${apiKey}`;
   const call = await fetch(cityApi);
   if (!call.ok) {
     const message = `An error has occured: ${call.status}`;
